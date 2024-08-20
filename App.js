d1eb24df1851cd00003bf691e6da2555b660c80d
SRV <vikneshsrv24@gmail.com>
2024-08-20 00:29:47 -0700

    Update app.js

---
+++ App.js
@@ -0,0 +1,7 @@
+import React,{useState,useEffect} from 'react'; // usestate->add state func component, useffect->functional
+import styled from 'styled-component'; // for css in the page.
+
+import HabitTracker from './components/HabitTracker';
+import PieChart from './components/PieChart';
+import Notes from './components/Notes';
+import EodM from './components/EodM';
